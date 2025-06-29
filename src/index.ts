1244dd2b7fb953ff5e638c538ab4d7a3dcaeda03
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-29 22:23:09 +0100

    :tada:

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { handle } from "hono/vercel";
 const app = new Hono().basePath("/api");
 
 app.get("/", (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel!" });
 });
 
 const handler = handle(app);
