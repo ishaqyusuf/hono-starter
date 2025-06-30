bb5a9ad9232ef0e0a874d0f27667016dc9966635
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-06-30 09:54:39 +0100

    asdsa

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
+import { appRouter } from "./trpc/routers/_app";
+import { createTRPCContext } from "./trpc/init";
 
 const app = new Hono().basePath("/api");
 app.get("/", (c) => {
@@ -6,7 +9,13 @@ app.get("/", (c) => {
     message: "Congrats! You've deployed Hono to Vercel! FROM SRC",
   });
 });
-
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: createTRPCContext,
+  })
+);
 export { app };
 const server = {
   port: +(process.env.PORT ?? 3000),
